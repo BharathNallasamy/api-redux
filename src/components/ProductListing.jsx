7afda86d6849f2505f2da0c6577c63087acb3f57
bharath2023ht <126450752+bharath2023ht@users.noreply.github.com>
2025-02-04 18:17:01 +0530

    first commit

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
 import axios from "axios";
+import { setProducts } from "../redux/actions/productActions";
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
-  console.log(products);
+  const dispatch = useDispatch();
 
   const fetchProducts = async () => {
     const response = await axios
@@ -13,12 +14,14 @@ const ProductListing = () => {
       .catch((err) => {
         console.log("Error:", err);
       });
-    console.log(response);
+    dispatch(setProducts(response.data));
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  console.log("Products:", products);
 
   return (
     <div className="ui grid container">
